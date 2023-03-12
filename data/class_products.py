88bd342e490dfe474818be2c3de1f5f1f4622bd4
Dmitry <k1791@mail.ru>
2023-03-12 19:51:53 +0500

    continue lesson2

--- data/class_products.py
+++ data/class_products.py
@@ -5,7 +5,7 @@ class Item ():
     pay_rate = 1
 
     def __init__(self, name: str, price, quantity):
-        self. = name  # наименование
+        self._name = name  # наименование
         self.price = price  # цена
         self.quantity = quantity  # кол-во
 
@@ -45,8 +45,8 @@ class Item ():
                 if Item.is_integer(row_quantity):
                     row_quantity = int(row_quantity)
 
-                i = cls(row_name, row_price, row_quantity).name
-        returnname
+                cls.all.append(cls(row_name, row_price, row_quantity))
+        return cls.all
 
     def calculate_total_price(self):
         """Метод расчета общей стоимости"""
