33418fdadd369f2f28037fc5737d40e0cd2c89f9
Dmitry <k1791@mail.ru>
2023-03-13 22:44:30 +0500

    continue lesson3

--- function/utils.py
+++ function/utils.py
@@ -1,25 +1,9 @@
 from data.class_products import Item
-from data.class_products import Phone
 
-# смартфон iPhone 14, цена 120_000, количетсво товара 5, симкарт 2
-phone2 = Item('iPhone 14', 120_000, 5)
-print(repr(phone2))
-
-phone1 = Phone('iPhone 14', 120_000, 5, 2)
-print(phone1)
-# iPhone 14
-print(repr(phone1))
-# Phone('iPhone 14', 120000, 5, 2)
-# print (phone1.number_of_sim)
-phone1.number_of_sim = 0
-print(repr(phone1))
-# ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.
-
-
-# item = Item('Телефон', 10000, 5)
-# item.name = 'Смартфон'
-# print(item.name)
-# # Смартфон
+item = Item('Телефон', 10000, 5)
+item.name = 'Смартфон'
+print(item.name)
+# Смартфон
 #
 #
 # print(repr(item))
