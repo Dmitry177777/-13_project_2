4897db985f0af5ded19b6e4ee4e13bcd2abf73b2
Dmitry <k1791@mail.ru>
2023-03-13 22:44:30 +0500

    continue lesson3

--- data/class_products.py
+++ data/class_products.py
@@ -76,6 +76,39 @@ class Item():
         return isInt  # True
 
 
+class Phone(Item):
+    """ новый наследуемый класс"""
+
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.number_of_sim_ = number_of_sim  # сим
+        # if isinstance(number_of_sim, int) and (number_of_sim > 0):
+        #     self.number_of_sim = number_of_sim  # сим
+        # else:
+        #     return ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+
+    @property
+    def number_of_sim(self):
+        return self.number_of_sim_
+
+    @number_of_sim.setter
+    def number_of_sim(self, new_number_of_sim):
+        if isinstance(new_number_of_sim, int) and new_number_of_sim > 0:
+            self.number_of_sim_ = new_number_of_sim  # сим
+            return
+        else:
+            return ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
+
+
+
+    def __repr__(self):
+        return f'Phone({self.name}, {self.price}, {self.quantity},{self.number_of_sim})'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
     __getattr__ = dict.get
